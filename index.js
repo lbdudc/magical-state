45029056f1db6e23b4f7166e65252f2cf735e6e1
victor <victor.lamas@udc.es>
2022-04-11 13:04:00 +0200

    Added index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+import Store from "./src/store";
+import InterfaceGetters from "./src/interface";
+import MSelector from "./src/components/mSelector";
+
+export { Store, InterfaceGetters, MSelector };
