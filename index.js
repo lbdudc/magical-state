aaf3bfc32f3dc9f807abe17970f75894519fb140
victor <victor.lamas@udc.es>
2022-04-28 12:51:11 +0200

    Fixed import on index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import MSelector from "./src/components/mSelector/mSelector";
 import MTimeline from "./src/components/mTimeline/TimelineComponent";
 import MDateFilter from "./src/components/mDateFilter/DateFilterComponent";
 import MListSelector from "./src/components/mListSelector/ListSelectorComponent";
-import MAutocomplete from "./src/components/mAutocomplete/MAutocompleteComponent";
+import MAutocomplete from "./src/components/mAutocomplete/mAutocompleteComponent";
 
 export { Store, InterfaceGetters, MSelector, MTimeline, MDateFilter, MListSelector, MAutocomplete };
