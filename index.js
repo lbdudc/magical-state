820c3f603c96659dca4973b45aa29bb2c1843438
victor <victor.lamas@udc.es>
2022-04-28 12:30:40 +0200

    feat: Added new component: mAutocomplete

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ import MSelector from "./src/components/mSelector/mSelector";
 import MTimeline from "./src/components/mTimeline/TimelineComponent";
 import MDateFilter from "./src/components/mDateFilter/DateFilterComponent";
 import MListSelector from "./src/components/mListSelector/ListSelectorComponent";
+import MAutocomplete from "./src/components/mAutocomplete/AutocompleteComponent";
 
-export { Store, InterfaceGetters, MSelector, MTimeline, MDateFilter, MListSelector };
+export { Store, InterfaceGetters, MSelector, MTimeline, MDateFilter, MListSelector, MAutocomplete };
