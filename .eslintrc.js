6cb66c807ea949beeaf30e7d0833d63c8234e4ed
Erick <erick.morales.pombo@udc.es>
2023-05-15 10:28:43 +0200

    Enabling automatic code formatting upon linting by updating package.json and enabling previously disabled eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,8 @@ module.exports = {
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
-    "no-fallthrough": "off",
     "no-case-declarations": "off",
     "no-async-promise-executor": "off",
-    "no-unused-vars": "off",
-    "vue/require-prop-type-constructor": "off",
     "no-useless-catch": "off",
-    "vue/require-explicit-emits": "off",
-    "vue/no-template-shadow": "off",
   },
 };
